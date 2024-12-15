f3d4bf0e18e1fdfaf7aac5dacd4adc7055da5f37
pprunty <pprunty@tcd.ie>
2024-12-15 15:52:06 +0000

    fix scripts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+};
 
 export default withNextIntl(nextConfig);
