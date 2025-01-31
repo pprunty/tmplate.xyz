e4bc8c48f986b7f839d4a777d3897b39b622498d
pprunty <pprunty@tcd.ie>
2025-01-31 16:29:16 +0000

    test

--- src/app/(social)/header.tsx
+++ src/app/(social)/header.tsx
@@ -1,4 +1,5 @@
 import CTA from "@/app/_layout/cta";
+import Logo from "../logo";
 
 export default function Header() {
   return (
@@ -14,24 +15,27 @@ export default function Header() {
         flex
         items-center
         justify-center
-        dark:bg-[#171717]/50
-        backdrop-blur-lg
-        sm:bg-primary-background-light/85 sm:dark:bg-[#171717]/85
+        sm:dark:bg-[#0D0D0D] sm:bg-[#FCFCFC]
+        backdrop-blur-xl
+        bg-primary-background-light/70 dark:bg-[#171717]/70
         border-b dark:border-[#333] border-[#EAEAEA]
         sm:border-0
       "
-    >
-      <div className="relative w-full flex flex-col">
-        {/* Center Home on Desktop */}
-        <div className="hidden sm:flex w-full justify-center">
-          <h3 className="text-md font-semibold dark:text-white">Home</h3>
-        </div>
+     >
+           <div className="relative w-full flex flex-col">
+             {/* Center Home on Desktop */}
+             <div className="hidden sm:flex w-full justify-center">
+               <h3 className="text-md font-semibold dark:text-white">Home</h3>
+             </div>
 
-        {/* CTA on mobile (above content) (hidden on md+) */}
-        <div className="sm:hidden px-2">
-          <CTA options={["auth"]} />
-        </div>
-      </div>
-    </header>
+             {/* Logo + CTA on Mobile (Hidden on md+) */}
+             <div className="sm:hidden px-2 flex flex-col items-center w-full">
+               <Logo className="mb-2" /> {/* Centered logo */}
+               <div className="w-full">
+                 <CTA options={["auth"]} /> {/* Full-width CTA */}
+               </div>
+             </div>
+           </div>
+         </header>
   );
 }
